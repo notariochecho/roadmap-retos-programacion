73a58649044f2d676e22689beae1d3355a10095e
notariochecho <notarioalv.cesar@gmail.com>
2024-05-21 15:05:29 -0300

    Create notariochecho.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/notariochecho.py
@@ -0,0 +1,29 @@
+# https://www.python.org
+
+"""
+Este es un comentario 
+con varias
+lineas 
+"""
+
+'''
+Este tambien es un comentario
+con varias 
+lineas 
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante"  # por convención
+MY_CONSTANT = "KJADJAHSDKJHAKSDJH"
+
+
+my_int: int= 1
+my_float = 1.5
+my_bool = True 
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print ("Hola, Phyton!")
